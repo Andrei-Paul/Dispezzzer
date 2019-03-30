04a14664b5e2453912eccc70300b634cd10c21f3
Ghitea Andrei Paul <ghitea.andrei.paul@gmail.com>
2019-03-30 23:45:49 +0200

    Added Message constants. Added commented trial Message constants.

--- Application/src/main/java/com/example/android/bluetoothchat/Constants.java
+++ Application/src/main/java/com/example/android/bluetoothchat/Constants.java
@@ -28,6 +28,14 @@ public interface Constants {
     public static final int MESSAGE_DEVICE_NAME = 4;
     public static final int MESSAGE_TOAST = 5;
 
+    public static final String MESSAGE_DELIMITER = "█";
+    public static final String MESSAGE_CONFIGURE = "CONFIGURE_DISPEZZZER";
+    public static final String MESSAGE_RING = "RING_DISPEZZZER";
+
+//    public static final String MESSAGE_DELIMITER = ":";
+//    public static final String MESSAGE_CONFIGURE = "CONF";
+//    public static final String MESSAGE_RING = "RING";
+
     // Key names received from the BluetoothChatService Handler
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
